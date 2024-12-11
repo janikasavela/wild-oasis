2a18f4f8d1bb49011188ffd06370dff6e349cde1
Janika Savela <janika_s@hotmail.com>
2024-12-11 18:41:28 +0200

    varauksien filtteröinti toimii

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -4,6 +4,7 @@ import CabinRow from './CabinRow'
 import { useCabins } from './useCabins'
 import Table from '../../ui/Table'
 import Menus from '../../ui/Menus'
+import Empty from '../../ui/Empty'
 import { useSearchParams } from 'react-router-dom'
 
 function CabinTable() {
@@ -11,6 +12,7 @@ function CabinTable() {
   const [searchParams] = useSearchParams()
 
   if (isLoading) return <Spinner />
+  if (!cabins.length) return <Empty resourceName='cabins' />
 
   // 1) Filter
 
